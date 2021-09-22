7a66153c8f77c76c69141cf441c7c413e3c4a922
malinovaya0iriska <malinovayairiska@gmail.com>
2021-09-22 08:56:27 +0300

    feat: share project on github

---
+++ assets/js/self-test.js
@@ -0,0 +1,30 @@
+console.log(`
+RS School - Stage 1 2021
+Self-Test: 
+1. Valid code- 10 points 
+2. Semantic layout - 20 points:
+    [⭐️] article
+    [⭐️] footer
+    [⭐️] header
+    [⭐️] main
+    [⭐️] nav
+    [⭐️] section
+    [⭐️] h1
+    [⭐️] h2
+    [⭐️] h3
+    [⭐️] h4
+3. Css styles - 10 points
+4. The content is centered, main background stretches - 10 points
+5. Adaptive layout - 10 points
+6. Burger menu - 10 points
+7. Image, proportions are not distorted - 10 points
+8. Contacts and skills are created by list <ul> - 10 points
+9. CV contains contacts, information about education, level of English - 10 points
+10. Example of my code with highlighting - 10 points
+11. List of projects (image, description, link) - 10 points
+12. CV in English - 10 points
+13. Pull request is correct - 10 points
+14. Video summary - 10 points
+15. Design - 10 points
+Total score: 160 points
+`);
